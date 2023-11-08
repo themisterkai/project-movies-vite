d57231a2be6fcfac47aa58de5f8f76c29f3f8f9e
Kris Albert Lee <krisalbert@gmail.com>
2023-11-08 21:22:46 +0100

    add HoverOverlay component

--- src/components/MoviePoster.jsx
+++ src/components/MoviePoster.jsx
@@ -1,21 +1,23 @@
 import { PropTypes } from 'prop-types';
+import { HoverOverlay } from './HoverOverlay';
 
 export const MoviePoster = ({
-  id,
+  // id,
   original_title: originalTitle,
   poster_path: posterPath,
   release_date: releaseDate,
 }) => {
   const posterImage = `https://image.tmdb.org/t/p/w500${posterPath}`;
   return (
-    <>
+    <div className="moviePosterContainer">
+      <HoverOverlay originalTitle={originalTitle} releaseDate={releaseDate} />
       <img src={posterImage} />
-    </>
+    </div>
   );
 };
 
 MoviePoster.propTypes = {
-  id: PropTypes.string.isRequired,
+  // id: PropTypes.string.isRequired,
   original_title: PropTypes.string.isRequired,
   poster_path: PropTypes.string.isRequired,
   release_date: PropTypes.string.isRequired,
